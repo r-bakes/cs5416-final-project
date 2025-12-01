5ceb6df745eeeb4e6df251f0b2f56731039f8646
Laurence Yang <yufanyang@berkeley.edu>
2025-12-01 18:04:42 -0500

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 echo "Running install.sh..."
 
-python3 -m venv .
+python3 -m venv .venv
 source .venv/bin/activate
-python3 pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Installation complete!"
 
